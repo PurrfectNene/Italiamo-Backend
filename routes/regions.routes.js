83270d7f9fcabdf6a5f40fb5c403a69fafac60cc
Marta El Bay <martaelbaygiberti@gmail.com>
2024-04-13 11:26:02 +0200

    Added relation between regions and places

--- routes/regions.routes.js
+++ routes/regions.routes.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 
 const Region= require('../models/Region.model')
 const City = require("../models/City.model");
+const Place = require("../models/Place.model")
 
 router.get("/regions", (req, res, next) =>{
 
@@ -31,6 +32,7 @@ router.get("/regions/:regionId", (req,res,next)=>{
 
 router.get("/regions/:regionId/cities", (req, res, next) => {
     const { regionId } = req.params;
+    console.log(regionId)
   
     if (!mongoose.Types.ObjectId.isValid(regionId)) {
       res.status(400).json({ message: "Specified id is not valid" });
@@ -39,10 +41,12 @@ router.get("/regions/:regionId/cities", (req, res, next) => {
   
     Region.findById(regionId)
       .then((region) => {
+        console.log(region)
         City.find({
           region: region._id,
         })
           .then((city) => {
+            console.log(city)
             res.json(city);
           })
           .catch((err) => next(err));
@@ -50,5 +54,36 @@ router.get("/regions/:regionId/cities", (req, res, next) => {
       .catch((err) => next(err));
   });
 
+
+  router.get("/regions/:regionId/places", (req, res, next) => {
+    const { regionId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(regionId)) {
+        res.status(400).json({ message: "Specified id is not valid" });
+        return;
+    }
+
+    Region.findById(regionId)
+        .then((region) => {
+            if (!region) {
+                res.status(404).json({ message: "Region not found" });
+                return;
+            }
+
+            City.find({ region: region._id })
+                .then((cities) => {
+                    const cityIds = cities.map(city => city._id);
+                  
+            
+                    Place.find({ city: { $in: cityIds } })
+                        .then((places) => {
+                            res.json(places);
+                        })
+                        .catch((err) => next(err));
+                })
+                .catch((err) => next(err));
+        })
+        .catch((err) => next(err));
+});
   
 module.exports = router
