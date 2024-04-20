9e6fe8dfaab46ee5c1a9322292f8d71b02d6c34e
Marta El Bay <martaelbaygiberti@gmail.com>
2024-04-20 17:25:52 +0200

    reviews model and routes

--- routes/places.routes.js
+++ routes/places.routes.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 
 const Place = require("../models/Place.model")
 const City = require("../models/City.model")
+const Review = require("../models/Review.model");
+
 
 router.get("/places", (req, res, next) => {
     Place.find()
@@ -84,9 +86,72 @@ router.delete("/places/:placeId", (req, res, next) => {
         })
 })
 
+router.post('/places/:placeId/reviews', (req, res) => {
+    const { userId, rating, review } = req.body;
+    Review.create({ user: userId, place: req.params.placeId, rating, review })
+      .then(newReview => {
+        res.status(201).json(newReview);
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
+  });
+
+
+  // Adding reviews here because they are linked with a specific place
+  // need to work on authentication
+  
+  // Get Reviews for a Place
+  router.get('/places/:placeId/reviews', (req, res) => {
+    Review.find({ place: req.params.placeId })
+      .then(reviews => {
+        res.json(reviews);
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
+  });
+  
+  // Get Review by ID
+  router.get('/places/:placeId/reviews/:reviewId', (req, res) => {
+    Review.findOne({ _id: req.params.reviewId, place: req.params.placeId })
+      .then(review => {
+        if (review) {
+          res.json(review);
+        } else {
+          res.status(404).json({ message: 'Review not found' });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
+  });
+  
+  // Update Review
+  router.put('/places/:placeId/reviews/:reviewId', (req, res) => {
+    const { rating, review } = req.body;
+    Review.findOneAndUpdate({ _id: req.params.reviewId, place: req.params.placeId }, { rating, review }, { new: true })
+      .then(updatedReview => {
+        res.json(updatedReview);
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
+  });
+  
+  // Delete Review
+  router.delete('/places/:placeId/reviews/:reviewId', (req, res) => {
+    Review.findOneAndDelete({ _id: req.params.reviewId, place: req.params.placeId })
+      .then(() => {
+        res.json({ message: 'Review deleted' });
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
+  });
+  
+  module.exports = router;
 
 
 
 
-
-module.exports = router
