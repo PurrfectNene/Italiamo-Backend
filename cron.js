15b0e9001e43a62fe2a2dd9976ee8711b3103519
Irene <amoreperdesi@gmail.com>
2024-10-09 19:39:15 +0200

    remove https and dev environment

--- cron.js
+++ cron.js
@@ -1,23 +1,16 @@
 // Cron job to hit endpoint every 5 min to keep backend alive always
 const cron = require("cron");
 const http = require("http");
-const https = require("https");
 
 // Load environment variables from .env file
 require("dotenv").config();
 
-// Determine if the environment is development or production
-const isDev = process.env.NODE_ENV === "development";
-
 // Define the backend URL based on the environment
 const backendUrl = `http://localhost:${process.env.PORT}/health`
 
-const job = new cron.CronJob("*/1 * * * *", function () {
+const job = new cron.CronJob("*/5 * * * *", function () {
   // This function will be executed every 5 minutes.
   console.log("Keeping the server alive");
-
-  // Use http for dev (localhost) and https for production (your domain)
-  const client = isDev ? http : https;
 
   // Perform an HTTPS GET request to hit any backend api.
   http
